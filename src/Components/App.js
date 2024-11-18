2cb00a9cece703de403dd24b562a838d27502049
Morino_Belgotaku <morinbruno@hotmail.be>
2024-11-18 02:27:30 +0100

    new file:   src/Components/App.js 	new file:   src/input.css 	new file:   src/output.css 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/index.js

---
+++ src/Components/App.js
@@ -0,0 +1,10 @@
+function App() {
+  
+  return (
+    <div className='flex justify-center items-center h-full w-full'>
+      <h1 className='text-red-600 font-bold text-7xl rounded-full p-5 border-8 border-red-600 bg-white'>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
