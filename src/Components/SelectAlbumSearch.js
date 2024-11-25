ef8c2bba4450106141dd569ff52fe4d685e0b505
Morino_Belgotaku <morinbruno@hotmail.be>
2024-11-25 13:33:38 +0100

    File update

---
+++ src/Components/SelectAlbumSearch.js
@@ -0,0 +1,9 @@
+const SelectAlbumSearch = () => {
+    return (
+        <div className="m-2 border-2 bg-[#A33634] rounded-lg shadow-lg border-warning bg-opacity-70">
+
+        </div>
+    )
+}
+
+export default SelectAlbumSearch;
