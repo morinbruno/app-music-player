85ad12a43ceceabf0e1b5005b17805927abee36e
Morino_Belgotaku <morinbruno@hotmail.be>
2024-11-25 13:34:07 +0100

    Update CSS

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,17 @@
 import PlayerMusic from "./PlayerMusic.js";
 import FormMusic from './FormMusic.js';
+import SelectAlbumSearch from "./SelectAlbumSearch.js";
+import ListMusic from "./ListMusic.js";
 
 function App() {
-  
   return (
-    <div className='flex flex-col justify-between items-center h-full w-full bg-cyan-950'>
+    <div className='flex flex-col justify-between items-center h-full w-full bg-gradient-to-br from-[#A33634] to-[#424290]'>
       <div className="grid grid-cols-1 lg:grid-cols-2 w-full">
         <FormMusic />
+        <SelectAlbumSearch />
+      </div>
+      <div className="grid grid-rows-1 w-full h-full">
+        <ListMusic />
       </div>
       <PlayerMusic />
     </div>
--- src/Components/FormMusic.js
+++ src/Components/FormMusic.js
@@ -3,39 +3,43 @@ import {useState} from 'react'
 
 const FormMusic = () => {
     return (
-        <div className="border-3 m-3 p-4 rounded-lg backdrop-blur bg-cyan-950 border-cyan-700 shadow-lg">
+        <div className="p-4 backdrop-blur m-2 border-2 bg-[#A33634] rounded-lg shadow-lg border-warning bg-opacity-70">
+            <h1 className="mb-4 text-xl">Ajouter une musique à la bibliothèque</h1>
             <form className="max-full">
                 <div className="mb-2">
                     <Input 
-                        color="primary"
+                        color="warning"
                         type="text"
                         label="Titre de la musique"
                     />
                 </div>
                 <div className="mb-2">
                     <Input 
-                        color="primary"
+                        color="warning"
                         type="text"
                         label="Titre de l'album"
                     />
                 </div>
                 <div className="mb-4">
                     <Input 
-                        color="primary"
+                        color="warning"
                         type="text"
                         label="Nom de l'artiste"
                     />
                 </div>
                 <div className="mb-4">
                     <Input 
-                        color="primary"
+                        color="warning"
                         type="file"
                         description="Format MP3"
+                        classNames={{
+                            description: "text-white"
+                        }}
                     />
                 </div>
                 <div>
                     <Button
-                        color="primary"
+                        color="warning"
                         className="w-full"
                     >
                         Ajouter
