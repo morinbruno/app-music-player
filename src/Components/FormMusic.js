48be0ea31b1174169f58dd5c67f0c068ba59caaa
Morino_Belgotaku <morinbruno@hotmail.be>
2024-12-20 16:26:21 +0100

    Check file is MP3

--- src/Components/FormMusic.js
+++ src/Components/FormMusic.js
@@ -1,7 +1,17 @@
 import {Input, Button} from "@nextui-org/react";
-import {useState} from 'react'
 
-const FormMusic = () => {
+const FormMusic = ({URLMusic, setURLMusic}) => {
+
+    const handleMusic = (e) => {
+        const file = e.target.files[0];
+        if (file && file.type === "audio/mpeg") {
+            const url = URL.createObjectURL(file)
+            setURLMusic(url);
+          } else {
+            alert("Veuillez sélectionner un fichier MP3 valide.");
+          }
+    }
+
     return (
         <div className="p-4 backdrop-blur m-2 border-2 bg-[#A33634] rounded-lg shadow-lg border-warning bg-opacity-70">
             <h1 className="mb-4 text-xl">Ajouter une musique à la bibliothèque</h1>
@@ -35,6 +45,7 @@ const FormMusic = () => {
                         classNames={{
                             description: "text-white"
                         }}
+                        onChange={handleMusic}
                     />
                 </div>
                 <div>
