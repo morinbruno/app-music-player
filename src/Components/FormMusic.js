3b8eaaf8075b19ff0060b83b55738e53acbaa6bb
Morino_Belgotaku <morinbruno@hotmail.be>
2024-12-22 16:56:50 +0100

    handle input

--- src/Components/FormMusic.js
+++ src/Components/FormMusic.js
@@ -1,19 +1,51 @@
 import {Input, Button} from "@nextui-org/react";
 
-const FormMusic = ({URLMusic, setURLMusic}) => {
+const FormMusic = ({title, setTitle, artist, setArtist, album, mp3File, setAlbum, setMp3File, addMusic, cover}) => {
 
     const handleMusic = (e) => {
         const file = e.target.files[0];
         if (file && file.type === "audio/mpeg") {
-            const url = URL.createObjectURL(file)
-            setURLMusic(url);
+            setMp3File(file);
           } else {
             alert("Veuillez sélectionner un fichier MP3 valide.");
           }
     }
 
+    const handleTitle = (e) => {
+        const titleValue = e.target.value;
+        if(/\d/.test(titleValue)){
+            alert("Le titre de la musique ne doit pas contenir de chiffres.");
+        } else if(titleValue.length > 50){
+            alert("Le titre de la musique ne doit pas dépasser 50 caractères.");
+        } else {
+            setTitle(titleValue);
+        }
+    }
+
+    const handleAlbum = (e) => {
+        const albumValue = e.target.value;
+        if(/\d/.test(albumValue)){
+            alert("Le titre de l'album ne doit pas contenir de chiffres.");
+        } else if(albumValue.length > 50){
+            alert("Le titre de l'album ne doit pas dépasser 50 caractères.");
+        } else {
+            setAlbum(albumValue);
+        }
+    }
+
+    const handleArtist = (e) => {
+        const artistValue = e.target.value;
+        if(/\d/.test(artistValue)){
+            alert("Le nom de l'artist ne doit pas contenir de chiffres.");
+        } else if(artistValue.length > 50){
+            alert("Le nom de l'artist ne doit pas dépasser 50 caractères.");
+        } else {
+            setArtist(artistValue);
+        }
+    }
+
     return (
-        <div className="p-4 backdrop-blur m-2 border-2 bg-[#A33634] rounded-lg shadow-lg border-warning bg-opacity-70">
+        <div className="p-4 backdrop-blur m-2 border-2 bg-[#A33634] rounded-lg shadow-lg border-warning bg-opacity-70 h-full">
             <h1 className="mb-4 text-xl">Ajouter une musique à la bibliothèque</h1>
             <form className="max-full">
                 <div className="mb-2">
@@ -21,6 +53,8 @@ const FormMusic = ({URLMusic, setURLMusic}) => {
                         color="warning"
                         type="text"
                         label="Titre de la musique"
+                        value={title}
+                        onChange={handleTitle}
                     />
                 </div>
                 <div className="mb-2">
@@ -28,6 +62,8 @@ const FormMusic = ({URLMusic, setURLMusic}) => {
                         color="warning"
                         type="text"
                         label="Titre de l'album"
+                        value={album}
+                        onChange={handleAlbum}
                     />
                 </div>
                 <div className="mb-4">
@@ -35,6 +71,8 @@ const FormMusic = ({URLMusic, setURLMusic}) => {
                         color="warning"
                         type="text"
                         label="Nom de l'artiste"
+                        value={artist}
+                        onChange={handleArtist}
                     />
                 </div>
                 <div className="mb-4">
@@ -52,6 +90,8 @@ const FormMusic = ({URLMusic, setURLMusic}) => {
                     <Button
                         color="warning"
                         className="w-full"
+                        onClick={addMusic}
+                        isDisabled={title === "" || album === "" || artist === "" || mp3File === "" || cover === ""}
                     >
                         Ajouter
                     </Button>
