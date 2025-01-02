30c7cdce3d19423717409b63e411a2454805c787
Morino_Belgotaku <morinbruno@hotmail.be>
2025-01-02 14:23:36 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/Components/App.js 	modified:   src/output.css

--- src/Components/App.js
+++ src/Components/App.js
@@ -43,10 +43,10 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col justify-between items-center h-full w-full bg-gradient-to-br from-[#A33634] to-[#424290]">
+    <div className="flex flex-col justify-between items-center h-full w-full">
       <AddMenu URLMusic={URLMusic} setURLMusic={setURLMusic} hideAddMenu={hideAddMenu} setPistes={setPistes} />
       <div className="grid grid-rows-1 w-full h-full">
-        <ListMusic openAddMenu={openAddMenu} pistes={pistes} URLMusic={URLMusic} setURLMusic={setURLMusic} setCover={setCover} setMusicSelected={setMusicSelected} />
+        <ListMusic openAddMenu={openAddMenu} pistes={pistes} URLMusic={URLMusic} setURLMusic={setURLMusic} setCover={setCover} cover={cover} setMusicSelected={setMusicSelected} />
       </div>
       <PlayerMusic URLMusic={URLMusic} setURLMusic={setURLMusic} cover={cover} setCover={setCover} musicSelected={musicSelected} setMusicSelected={setMusicSelected} pistes={pistes}/>
     </div>
